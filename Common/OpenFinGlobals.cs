57a163bef2dafa12ca5d9709b98d0ccf1de5e78a
JohnathonLamOF <johnathon@openfin.co>
2018-10-31 09:19:00 +0000

    Changed url to another angular app

--- Common/OpenFinGlobals.cs
+++ Common/OpenFinGlobals.cs
@@ -13,13 +13,15 @@ namespace RapidLaunch.Common
         {
             var openfinOptions = new Fin.RuntimeOptions()
             {
-                Version = "9.*"
+                Version = "9.*",
+                PortDiscoveryMode = Fin.PortDiscoveryMode.WindowClass
             };
 
             RuntimeInstance = Fin.Runtime.GetRuntimeInstance(openfinOptions);
+            // You can replace with your
             //DefaultAppUrl = "http://akveo.com/blur-admin/#/dashboard";
-            DefaultAppUrl = "http://google.com";
-            //DefaultAppUrl = "http://flatlogic.github.io/angular-material-dashboard/#/dashboard";
+            //DefaultAppUrl = "http://google.com";
+            DefaultAppUrl = "http://flatlogic.github.io/angular-material-dashboard/#/dashboard";
         }
 
         public static Fin.Runtime RuntimeInstance { get; private set; }
